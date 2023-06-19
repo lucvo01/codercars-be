0389a22a2b0902deb2f93914c085723c0318c338
Luc Vo <luke.syvo@gmail.com>
2023-06-19 13:58:13 -0600

    .

--- controllers/car.controller.js
+++ controllers/car.controller.js
@@ -1,37 +1,91 @@
-const mongoose = require('mongoose');
-const Car = require('../models/Car');
+const mongoose = require("mongoose");
+const Car = require("../models/Car");
+const { sendResponse, AppError } = require("../helpers/utils");
 const carController = {};
 
 carController.createCar = async (req, res, next) => {
-	try {
-		// YOUR CODE HERE
-	} catch (err) {
-		// YOUR CODE HERE
-	}
+  try {
+    // YOUR CODE HERE
+    const info = req.body;
+    if (!info) throw new AppError(401, "Bad Request", "Create car Error");
+
+    const created = await car.create(info);
+    sendResponse(
+      res,
+      200,
+      true,
+      { car: created },
+      null,
+      "Create Car Successfully"
+    );
+  } catch (err) {
+    // YOUR CODE HERE
+    next(err);
+  }
 };
 
 carController.getCars = async (req, res, next) => {
-	try {
-		// YOUR CODE HERE
-	} catch (err) {
-		// YOUR CODE HERE
-	}
+  try {
+    // YOUR CODE HERE
+    const filter = req.query;
+    const listOfFound = await Car.find({}).limit(2);
+    sendResponse(
+      res,
+      200,
+      true,
+      "Get Car List Successfully!",
+      { cars: listOfFound, page: 1, total: 11914 },
+      null
+    );
+  } catch (err) {
+    // YOUR CODE HERE
+    next(err);
+  }
 };
 
 carController.editCar = async (req, res, next) => {
-	try {
-		// YOUR CODE HERE
-	} catch (err) {
-		// YOUR CODE HERE
-	}
+  try {
+    // YOUR CODE HERE
+    const targetId = req.body.targetId;
+    const updateInfo = req.body.updateInfo;
+    const options = { new: true };
+
+    const updated = await car.findByIdAndUpdate(targetId, updateInfo, options);
+
+    sendResponse(
+      res,
+      200,
+      true,
+      { car: updated },
+      null,
+      "Update Car Successfully!"
+    );
+  } catch (err) {
+    // YOUR CODE HERE
+    next(err);
+  }
 };
 
 carController.deleteCar = async (req, res, next) => {
-	try {
-		// YOUR CODE HERE
-	} catch (err) {
-		// YOUR CODE HERE
-	}
+  try {
+    // YOUR CODE HERE
+    const targetId = req.params.id;
+    const options = { new: true };
+
+    const updated = await car.findByIdAndDelete(targetId, options);
+
+    sendResponse(
+      res,
+      200,
+      true,
+      { car: updated },
+      null,
+      "Delete Car Successfully!"
+    );
+  } catch (err) {
+    // YOUR CODE HERE
+    next(err);
+  }
 };
 
 module.exports = carController;
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,35 @@
-const express = require('express');
+const { sendResponse, AppError } = require("../helpers/utils.js");
+const express = require("express");
 const router = express.Router();
 
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  res.status(200).send("Welcome to CoderSchool!");
+});
+
+router.get("/template/:test", async (req, res, next) => {
+  const { test } = req.params;
+  try {
+    //turn on to test error handling
+    if (test === "error") {
+      throw new AppError(401, "Access denied", "Authentication Error");
+    } else {
+      sendResponse(
+        res,
+        200,
+        true,
+        { data: "template" },
+        null,
+        "template success"
+      );
+    }
+  } catch (err) {
+    next(err);
+  }
+});
+
 // CAR
-const carAPI = require('./car.api');
-router.use('/car', carAPI);
+const carRouter = require("./car.api.js");
+router.use("/cars", carRouter);
 
 module.exports = router;
